f2697eec7b822d18ae53187280a3d2ca326c5f7a
marselosgcc <89476116+Marcelosgc1@users.noreply.github.com>
2024-04-13 19:55:51 -0300

    Comentários explicando inserção

--- Exercícios/exercicio1.c
+++ Exercícios/exercicio1.c
@@ -23,17 +23,44 @@ int insert(struct node **head, int n){
     new->numero = n;
     new->next = NULL;
 
+    /*
+    Se a HEAD for NULL, ou seja, nenhuma struct node existir ainda,
+    então a struct node 'new' é atribuida à head, 
+    como primeiro elemento da lista encadeada.
+    */
 
     if (*head==NULL){
         *head=new;
         return 0;
     }
 
+    /*
+    Se o número do primeiro elemento da lista encadeada for maior
+    que o número do elemento que queremos pôr na lista, então
+    fazemos a 'new->next' apontar para o primeiro elemento, e fazemos
+    a head apontar para 'new', dessa forma, o primeiro elemento da
+    lista encadeada passa da ser a 'new'.
+    */
+
     if (new->numero<=aux->numero){
         new->next = aux;
         *head = new;
         return 0;
     }
+
+    /*
+    Se não for nenhum dos casos acima, então começamos a percorrer
+    os elementos da lista encadeada. O programa vai pegar o número
+    do elemento à frente da auxiliar, (ou seja, se a auxiliar aponta 
+    para o primeiro elemento, o programa vai pegar o segundo elemento)
+    e verificar se é maior que o número da struct node 'new' que foi
+    criada. Se for o caso, a 'new' passa a apontar pra esse elemento 
+    que está na frente e a struct node que a 'aux' aponta passa a 
+    apontar para a 'new', dessa forma, inserindo o elemento no meio
+    da lista. Se não for o caso, então a 'aux' começa a apontar para
+    o próximo elemento da lista e refaz a avaliação do valor, percorrendo
+    a lista até achar um valor maior que a do número da new.    
+    */
 
     while (aux->next)
     {
@@ -44,6 +71,13 @@ int insert(struct node **head, int n){
         }
         aux = aux->next;
     }
+
+    /*
+    Se toda lista for percorrida, e não for achado nenhum valor maior
+    que o número da new, então a struct node 'new' é inserida no fim
+    da lista encadeada.
+    */
+
     aux->next = new;
     return 0;
 
